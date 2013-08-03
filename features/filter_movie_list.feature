714ea2525fc8761f09092ab0e96a21f571f0962b
c0d3r85 <c0d3r85@gmail.com>
2013-08-03 11:49:30 +0700

    hw3 done

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,14 +22,17 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+	When I check the following ratings: PG, R
+	And I uncheck the following ratings: G, PG-13
+	And I press "Refresh"
+	Then I should see movies: "The Incredibles", "Raiders of the Lost Ark", "Amelie", "When Harry Met Sally", "The Terminator"
+	And I should not see movies: "Aladdin", "2001: A Space Odyssey", "Chicken Run", "The Help", "Chocolat"
 
 Scenario: no ratings selected
-  # see assignment
+	When I uncheck the following ratings: G, R, PG-13, PG
+	Then I should see all of the movies
 
 Scenario: all ratings selected
-  # see assignment
+	Given I check the following ratings: G, PG, R
+	Then I should see all of the movies
+
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+	Movie.create(movie)
   end
-  flunk "Unimplemented"
+  # flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,13 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
+	assert page.body.index(e1) < page.body.index(e2)
+end
+
+Then /I should see movies in this order: (.*)/ do |movies_string|
+	movies_indexes = movies_string.slice(1, movies_string.size - 2).split(/",\s*"/).map{ |movie| page.body.index(movie)}
+	assert movies_indexes == movies_indexes.sort
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +32,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+	rating_list.split(/,\s*/).each{|item| step "I #{uncheck}check \"ratings_#{item}\""}
+end
+
+Then /I should see all of the movies/ do
+	Movie.all.each{|item| step "I should see \"#{item.title}\""}
+end
+
+Then /I should( not)? see movies: (.*)/ do |visible, movies_list|
+	movies_list.slice(1, movies_list.size - 2).split(/",\s*"/).each{|item| step "I should#{visible} see \"#{item}\"" }
 end
